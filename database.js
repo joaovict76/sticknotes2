7c40ac883b64c28b902a815250136c4a04bf83c5
JOAO VICTOR DA SILVA GUILHERME <joao.vsguilherme@senacsp.edu.br>
2025-05-06 19:56:55 -0300

    publicação versão 1.0

--- database.js
+++ database.js
@@ -2,7 +2,7 @@
 const { connect } = require('http2')
 const mongoose = require('mongoose')
 
-const url = 'mongodb+srv://admin:123Senac@cluster02.czib3.mongodb.net/dbnotes'
+const url = 'mongodb://localhost:27017'
 
 let connected = false
 
